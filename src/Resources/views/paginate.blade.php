d2a0c3df1adbe5bbb1c28b11aabdce31eb403b2a
Roger Mathews Arruda <roger@interative.info>
2019-05-13 14:05:53 -0300

    Appends pagination

--- src/Resources/views/paginate.blade.php
+++ src/Resources/views/paginate.blade.php
@@ -1,8 +1,12 @@
 <div class="row mt-5 align-items-center">
-	<div class="col-6 clearfix">
-		{{$itens->total()}} registros encontrados.
-	</div>
-	<div class="col-6 text-right">
-		{{ $itens->render("pagination::bootstrap-4") }}
-	</div>
+    <div class="col-6 clearfix">
+        {{$itens->total()}} registros encontrados.
+    </div>
+    <div class="col-6 text-right">
+        @if (isset($appends))
+            {{ $itens->appends($appends)->render("pagination::bootstrap-4") }}
+        @else
+            {{ $itens->render("pagination::bootstrap-4") }}
+        @endif
+    </div>
 </div>
