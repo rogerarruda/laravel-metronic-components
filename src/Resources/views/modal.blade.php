2a5291ff4ae5d50deaa6f74dc510ed2f620063c0
Roger Mathews Arruda <roger@interative.info>
2020-03-03 15:44:03 -0300

    Correção verificação do tamanho do modal

--- src/Resources/views/modal.blade.php
+++ src/Resources/views/modal.blade.php
@@ -1,6 +1,6 @@
 <!--begin::Modal {{$id}}-->
 <div class="modal fade" id="{{$id}}" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog modal-dialog-centered {{ isset($size) && $size = 'sm' ? 'modal-sm' : '' }}{{ isset($size) && $size = 'lg' ? 'modal-lg' : '' }}" role="document">
+    <div class="modal-dialog modal-dialog-centered {{ isset($size) && $size == 'sm' ? 'modal-sm' : '' }}{{ isset($size) && $size == 'lg' ? 'modal-lg' : '' }}" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modal">{{ $header }}</h5>
